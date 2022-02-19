ea63f81cb69928e785bb5b4a63753ba5a5e098fd
marcelowos <marcelo.steler@gmail.com>
2022-02-19 16:12:45 -0300

    fist commit new notebook

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = '    http://api.weatherstack.com/current?access_key=c8e04634be2edc27e0158adf9bb67c6b&query=' + latitude + ',' + longitude + '&units=f'   
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out. It feels like " + body.current.feelslike + " degrees out. The Humidity is " + body.current.humidity)
         }
     })
 }
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoibWFyd3MiLCJhIjoiY2t6dTU5YjBhMDNtcjJ2bXlobXV3dWswNiJ9.weqLxwjdifnfcByRDcR3HA&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -13,7 +13,7 @@ const geocode = (address, callback) => {
                 latitude: body.features[0].center[1],
                 longitude: body.features[0].center[0],
                 location: body.features[0].place_name
-            })
+            }) 
         }
     })
 }
